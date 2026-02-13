0576a73da057f2a07d0d27c7bfb6692dccb3b5a0
Sanjeev Midda <s.midda1@gmail.com>
2026-02-13 20:35:41 +0000

    created quiz UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  // create question and answers
+  // track answers
+  // show score at end
+
+  //Create UI
+  return (
+    <div className="App">
+      <div className="mainWrapper">
+        <h1>Q.1</h1>
+        <div className="questionWrapper"></div>
+        <div className="answerWrapper"></div>
+        <button>S</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
