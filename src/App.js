61fa30beac67ace50d10fda0aed44516060d6d5d
Sanjeev Midda <s.midda1@gmail.com>
2026-02-13 20:50:06 +0000

    created data for questions

--- src/App.js
+++ src/App.js
@@ -5,6 +5,48 @@ function App() {
   // track answers
   // show score at end
 
+  const QUESTIONS = [
+    {
+      question: "What is the capital of France?",
+      answer: "Paris",
+      options: ["Lyon", "Marseille", "Paris", "Nice"],
+    },
+    {
+      question: "What is the capital of Japan?",
+      answer: "Tokyo",
+      options: ["Osaka", "Kyoto", "Hiroshima", "Tokyo"],
+    },
+    {
+      question: "What is the capital of Brazil?",
+      answer: "Brasilia",
+      options: ["Rio de Janeiro", "São Paulo", "Salvador", "Brasilia"],
+    },
+    {
+      question: "WWhat is the capital of Canada?",
+      answer: "Ottawa",
+      options: ["Toronto", "Vancouver", "Ottawa", "Montreal"],
+    },
+    {
+      question: "What is the capital of Australia?",
+      answer: "Canberra",
+      options: ["Sydney", "Melbourne", "Brisbane", "Canberra"],
+    },
+    {
+      question: "What is the capital of Egypt?",
+      answer: "Cairo",
+      options: ["Alexandria", "Giza", "Luxor", "Cairo"],
+    },
+    {
+      question: "What is the capital of South Africa?",
+      answer: "Pretoria",
+      options: ["Johannesburg", "Durban", "Pretoria", "Cape Town"],
+    },
+    {
+      question: "What is the capital of Argentina?",
+      answer: "Paris",
+      options: ["Córdoba", "Rosario", "Mendoza", "Buenos Aires"],
+    },
+  ];
   //Create UI
   return (
     <div className="App">
